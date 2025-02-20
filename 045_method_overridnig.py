96600c6b0292d636d7639c96253ed1cd159b96e4
Noname963 <cys3553@gmail.com>
2025-02-20 12:16:08 +0900

    rename

--- 045_method_overridnig.py
+++ 045_method_overridnig.py
@@ -3,5 +3,9 @@ class Animal:
         print("This animal is eating")
 
 class Rabbit(Animal):
-    pass
+    def eat(self):
+        print("This rabbit is eating a carrot")
+
+rabbit = Rabbit()
+rabbit.eat()
 
